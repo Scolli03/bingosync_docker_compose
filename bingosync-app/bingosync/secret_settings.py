2a0200cabbfd2fc7932516bbc6d9e335a8320155
Shain Collins <shainc@j-ldimensional.com>
2022-02-04 09:50:31 -0600

    setup for dev server

---
+++ bingosync-app/bingosync/secret_settings.py
@@ -0,0 +1,4 @@
+SECRET_KEY="%h^4as^=_fp^2!5_(h0yz&q*gjw&kiw*li$=c&67l%wgd_v^#n"
+ADMINS=[('John', 'john@example.com'), ('Mary', 'mary@example.com')]
+SERVER_EMAIL='root@localhost'
+DB_USER="postgres"
